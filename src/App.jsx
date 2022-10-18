988d724a6ce11dcd483d551c6fb7cf267505a8e3
cmhleveret <cmhleveret@gmail.com>
2022-10-18 11:28:31 +0100

    removal of checkout files and comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Button, Navbar } from 'react-bootstrap';
-import './App.css'
 import { Routes, Route, Link } from 'react-router-dom';
-import Checkout from './Checkout.jsx'
+import './App.css'
+
 
 const App = () => (
   <div>
@@ -9,27 +9,13 @@ const App = () => (
       <Navbar.Brand> PIZZA </Navbar.Brand>
       <Button>🍕</Button>
       <Link className='nav-link' to="/">Home</Link>
-      <Link className='nav-link' to="/CreatePizza">CreatePizza</Link>
-      <Link className='nav-link' to="/Orders">Orders</Link>
-      <Link className='nav-link' to="/Checkout">Checkout</Link>
     </Navbar>
 
 
     <Routes>
     <Route path="/" 
       />
-
-      <Route path="/CreatePizza" element={
-        <CreatePizza />}
-      />
-
-      <Route path="/Orders" element={
-        <Orders />}
-      />
-
-      <Route path="/Checkout" element={
-        <Checkout />}
-      />
+      
     </Routes>
   </div>
 )
