e976d4fd7ce6ec56fccfa3ecc0542075f7074c1b
DougieWesson <112854430+DougieWesson@users.noreply.github.com>
2022-10-21 13:46:07 +0100

    home page basket (#12)
    Co-authored-by: EyUp Student <eyupstudent@EyUps-MacBook-Air.local>

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import { useState } from "react";
-import { Routes, Route, Link } from 'react-router-dom';
-
+import { Routes, Route, Link } from "react-router-dom";
 
 import { Container, Nav, Navbar } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import './App.css';
-import './ToppingsList.css';
-import logo from "./img/dojopza.png"
-import CreatePizza from './CreatePizza';
-import Checkout from './Checkout.jsx'
+import "./App.css";
+import "./ToppingsList.css";
+import logo from "./img/dojopza.png";
+import CreatePizza from "./CreatePizza";
+import Checkout from "./Checkout.jsx";
 import Orders from "./Orders";
 import ViewOrder from "./ViewOrder";
 import ViewPizza from "./ViewPizza";
-import Basket from './Basket';
+import Basket from "./Basket";
 
 const App = () => {
-  const [orders, changeOrders] = useState(JSON.parse(localStorage.getItem("orders")) || []);
+  const [orders, changeOrders] = useState(
+    JSON.parse(localStorage.getItem("orders")) || []
+  );
   const [basket, changeBasket] = useState(
     JSON.parse(localStorage.getItem("basket")) || []
   );
@@ -25,7 +26,7 @@ const App = () => {
   return (
     <Container>
       <Navbar className="navbarDiv" bg="dark" variant="dark" expand="md">
-          <img src={logo} alt="logo" className="logo"/>
+        <img src={logo} alt="logo" className="logo" />
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
@@ -35,17 +36,32 @@ const App = () => {
             <Link className="nav-link" to="/orders">
               Orders
             </Link>
-            <Link className='nav-link' to="/createpizza">Create Pizza</Link>
+            <Link className="nav-link" to="/createpizza">
+              Create Pizza
+            </Link>
             <Link className="nav-link" to="/basket">
               Basket
             </Link>
-            <Link className='nav-link' to="/Checkout">Checkout</Link>
+            <Link className="nav-link" to="/Checkout">
+              Checkout
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      
+
       <Routes>
-        <Route path="/"/>
+        <Route
+          path="/"
+          element={
+            <div className="homePageBasket">
+              <Basket
+                basket={basket}
+                changeBasket={changeBasket}
+                changePizzaToEdit={changePizzaToEdit}
+              />
+            </div>
+          }
+        />
         <Route path="/orders" element={<Orders orders={orders} />} />
         <Route
           path="/vieworder/:orderId"
@@ -55,7 +71,8 @@ const App = () => {
           path="/vieworder/:orderId/:pizzaId"
           element={<ViewPizza orders={orders} />}
         />
-        <Route path="/createpizza"
+        <Route
+          path="/createpizza"
           element={
             <CreatePizza
               basket={basket}
@@ -65,17 +82,26 @@ const App = () => {
             />
           }
         />
-        <Route path="/basket" element={
-          <Basket
-            basket={basket}
-            changeBasket={changeBasket}
-            changePizzaToEdit={changePizzaToEdit}
-          />
-        }
+        <Route
+          path="/basket"
+          element={
+            <Basket
+              basket={basket}
+              changeBasket={changeBasket}
+              changePizzaToEdit={changePizzaToEdit}
+            />
+          }
         />
-        <Route path="/Checkout" element={
-          <Checkout basket={basket} changeBasket={changeBasket} orders={orders} changeOrders={changeOrders} />
-        }
+        <Route
+          path="/Checkout"
+          element={
+            <Checkout
+              basket={basket}
+              changeBasket={changeBasket}
+              orders={orders}
+              changeOrders={changeOrders}
+            />
+          }
         />
       </Routes>
     </Container>
